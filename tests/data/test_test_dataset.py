3a1e71c75ea42ebcb2d65024c339d1b4423445e1
Andrew <begineconst@gmail.com>
2023-12-08 05:06:08 +0200

    Data Test Adjustments.

--- tests/data/test_test_dataset.py
+++ tests/data/test_test_dataset.py
@@ -10,8 +10,6 @@ def test_dataset(test_df):
     test_df.expect_column_values_to_not_be_null(column="comment_text")
     # unique values
     test_df.expect_column_values_to_be_unique(column="id")
-    # type adherence
-    test_df.expect_column_values_to_be_of_type(column="comment_text", type_="str")
 
     # Expectation suite
     expectation_suite = test_df.get_expectation_suite(discard_failed_expectations=False)
