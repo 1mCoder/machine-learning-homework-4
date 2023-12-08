0824c44aa64c59b8cf204fe62ee13419d103e3ad
Andrew <begineconst@gmail.com>
2023-12-08 06:56:24 +0200

    I'm getting closer. Attempt to fix filepaths.

--- src/code/data.py
+++ src/code/data.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 def get_test_data():
-    return pd.read_csv('../../kmaml223/test.csv')
+    return pd.read_csv('kmaml223/test.csv')
 
 def get_train_data():
-    return pd.read_csv('../../kmaml223/train.csv')
+    return pd.read_csv('kmaml223/train.csv')
+
+if __name__ == "__main__":
+    get_test_data()
+    get_train_data()
